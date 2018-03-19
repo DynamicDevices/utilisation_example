8c6ab43ad19b774ba2eaf1d0cfa9542873813856
Alex J Lennon <ajlennon@dynamicdevices.co.uk>
2018-03-19 22:10:45 +0000

    utilisation: update copyright date to 2018
    Signed-off-by: Alex J Lennon <ajlennon@dynamicdevices.co.uk>

--- utilisation.c
+++ utilisation.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Dynamic Devices Ltd <info@dynamicdevices.co.uk>
+ * Copyright 2018 Dynamic Devices Ltd <info@dynamicdevices.co.uk>
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
